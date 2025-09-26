c1000b00177d7a1d3f7786f5b23344383a8781e6
nanomaoli <38492861+nanomaoli@users.noreply.github.com>
2025-09-26 19:55:13 +0000

    Deploying to gh-pages from @ nanomaoli/nanomaoli.github.io@9aef38b0500f4d3d979ccb50c375c10f5a18ffa5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -379,21 +379,21 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "books-",
-          title: '',
+      },{id: "books-uyghur-an-elementary-textbook",
+          title: 'Uyghur An Elementary Textbook',
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-one-paper-has-been-accepted-to-isca-2025",
+          title: 'One paper has been accepted to ISCA 2025!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
