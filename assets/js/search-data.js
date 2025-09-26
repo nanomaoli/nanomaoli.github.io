afb81418dbef876281f709e5f961b8e9e3f9a4d3
nanomaoli <38492861+nanomaoli@users.noreply.github.com>
2025-09-26 20:42:46 +0000

    Deploying to gh-pages from @ nanomaoli/nanomaoli.github.io@f43cd138a326c247664406d3abb6c30ba41aaf17 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -435,7 +435,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6C%69%30%30%33%37%30%33@%75%6D%6E.%65%64%75", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/nanomaoli", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -445,18 +452,18 @@ ninja.data = [{
           window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/hao-li-622a371a9/", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=lMjfYqoAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
