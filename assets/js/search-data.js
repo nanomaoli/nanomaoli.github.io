405b7873e47c8b3a6cec4fd96b84236ae9b09f18
nanomaoli <38492861+nanomaoli@users.noreply.github.com>
2025-09-26 19:24:48 +0000

    Deploying to gh-pages from @ nanomaoli/nanomaoli.github.io@beb433e4388e607c976196489a3e698a43fb9abf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -379,8 +379,8 @@ ninja.data = [{
             window.location.href = "/blog/2015/formatting-and-links/";
           
         },
-      },{id: "books-",
-          title: '',
+      },{id: "books-uyghur-an-elementary-textbook",
+          title: 'Uyghur An Elementary Textbook',
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
