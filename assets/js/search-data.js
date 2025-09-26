352c4a2d84cdb32ec502f5ee539126d01a1d097c
nanomaoli <38492861+nanomaoli@users.noreply.github.com>
2025-09-26 20:07:07 +0000

    Deploying to gh-pages from @ nanomaoli/nanomaoli.github.io@ea571b1693d9624551cd70354728919c70b9bffc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -384,11 +384,6 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-one-paper-has-been-accepted-to-isca-2025",
           title: 'One paper has been accepted to ISCA 2025!',
           description: "",
